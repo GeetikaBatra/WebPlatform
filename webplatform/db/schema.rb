8c9ee7a8a81676d937cad3cec133bf5e7f83d4e1
dominikam <dominika.mips@gmail.com>
2015-08-10 23:26:30 +0200

    10 - create mentor

--- webplatform/db/schema.rb
+++ webplatform/db/schema.rb
@@ -11,9 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150810205051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mentors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "gender"
+    t.string   "country"
+    t.string   "program_country"
+    t.string   "time_zone"
+    t.text     "motivation"
+    t.string   "english_level"
+    t.string   "mentee_level",                     default: [],    array: true
+    t.boolean  "experienced",                      default: false
+    t.text     "mentor_experience"
+    t.text     "background"
+    t.boolean  "git",                              default: false
+    t.string   "programming_languages",            default: [],    array: true
+    t.text     "programming_experience"
+    t.text     "application_idea"
+    t.text     "concept_explanation"
+    t.integer  "time_availability",      limit: 2
+    t.string   "engagements",                      default: [],    array: true
+    t.string   "sources",                          default: [],    array: true
+    t.string   "build_step"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
